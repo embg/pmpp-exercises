b76ea213566bec6eaa9153e73670955633f8272e
Elliot Gorokhovsky <embg@fb.com>
2024-07-21 19:24:34 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
+setup(
+    name="chap3_kernels",
+    ext_modules=[
+        CUDAExtension(
+            "chap3_kernels",
+            [
+                "chap3_kernels.cpp",
+                "chap3_kernels.cu",
+            ],
+            libraries=["cuda"]
+        )
+    ],
+    cmdclass={"build_ext": BuildExtension.with_options(no_python_abi_suffix=True)},
+)
